09152e3642c7d8cdf793395aacedf486fc406bc5
Anan Zhuang <ananzh@amazon.com>
2021-08-06 06:57:27 +0000

    replace elastic url to opensearch url
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- scripts/kibana-docker.sh
+++ scripts/kibana-docker.sh
@@ -2,7 +2,7 @@
 
 exec docker run \
   --rm \
-  -e ELASTICSEARCH_URL="http://elasticsearch:9200" \
+  -e ELASTICSEARCH_URL="http://opensearch:9200" \
   -p 5601:5601 \
   --network=elastic \
   docker.elastic.co/kibana/kibana:7.0.0

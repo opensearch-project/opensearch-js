1452991547ccb3a645949e4a6fb0020c7ec7bd55
huuyafwww <huuya1234fwww@gmail.com>
2022-03-23 06:10:09 +0900

    The proper type was Action, not TDocument.
    Signed-off-by: huuyafwww <huuya1234fwww@gmail.com>

--- lib/Helpers.d.ts
+++ lib/Helpers.d.ts
@@ -115,7 +115,7 @@ export interface OnDropDocument<TDocument = unknown> {
       reason: string
     }
   }
-  operation: TDocument
+  operation: Action
   document: TDocument
   retried: boolean
 }

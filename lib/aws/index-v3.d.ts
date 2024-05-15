0a67df005f30d1ba6939b4cf6a2daa2ded30924d
maiconcarraro <maicon315@gmail.com>
2024-05-15 19:31:20 -0300

    missing type definitions for aws-v3
    Signed-off-by: maiconcarraro <maicon315@gmail.com>

---
+++ lib/aws/index-v3.d.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
+
+/// <reference types="node" />
+
+import {
+  AwsSigv4Signer,
+  AwsSigv4SignerError,
+  AwsSigv4SignerOptions,
+  AwsSigv4SignerResponse,
+} from './index';
+
+export { AwsSigv4Signer, AwsSigv4SignerOptions, AwsSigv4SignerResponse, AwsSigv4SignerError };

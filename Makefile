69154cd166e1ae38c39120daf3aba8022dbf743c
Theo Truong <theotr@amazon.com>
2024-08-20 12:20:20 -0600

    # Moved waiting logic to scripts
    Signed-off-by: Theo Truong <theotr@amazon.com>

--- Makefile
+++ Makefile
@@ -2,27 +2,8 @@ cluster.opensearch.build:
 	docker compose --project-directory .ci/opensearch build;
 
 cluster.opensearch.start:
-	docker compose --project-directory .ci/opensearch up -d; \
-	ready=false; \
-	for attempt in {1..20}; do \
-		echo '=====> waiting...'; \
-		sleep 5; \
-		if [ "$$SECURE_INTEGRATION" = "true" ]; then \
-			if curl -s -k https://localhost:9200; then \
-				ready=true; \
-				echo '----- Secured cluster ready' && break; \
-			fi; \
-		else \
-			if curl -s http://localhost:9200; then \
-				ready=true; \
-				echo '-----  Unsecured cluster ready' && break; \
-			fi; \
-		fi; \
-	done; \
-	if [ "$$ready" = "false" ]; then \
-		echo '----- Timeout waiting for cluster to be ready'; \
-    	exit 1; \
-    fi
+	docker compose --project-directory .ci/opensearch up -d;
+	bash ./scripts/wait-cluster.sh;
 
 cluster.opensearch.stop:
 	docker compose --project-directory .ci/opensearch down;
--- scripts/wait-cluster.sh
+++ scripts/wait-cluster.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-TEST_OPENSEARCH_SERVER=${TEST_OPENSEARCH_SERVER:-"http://localhost:9200"}
+for attempt in {1..20}; do
+  echo "----- Waiting... $attempt";
+  sleep 5; \
+  if [ "$SECURE_INTEGRATION" = "true" ]; then
+    if curl -s -k https://localhost:9200; then
+      echo '----- Secured cluster ready' && exit 0;
+    fi;
+  else
+    if curl -s http://localhost:9200; then
+      echo '-----  Unsecured cluster ready' && exit 0;
+    fi;
+  fi;
+done;
 
-attempt_counter=0
-max_attempts=5
-url="${TEST_OPENSEARCH_SERVER}/_cluster/health?wait_for_status=green&timeout=50s"
-
-echo "Waiting for OpenSearch..."
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' --max-time 55 "$url")" != "200" ]]; do
-  if [ ${attempt_counter} -eq ${max_attempts} ];then
-    echo "\nCouldn't connect to OpenSearch"
-    exit 1
-  fi
-
-  printf '.'
-  attempt_counter=$(($attempt_counter+1))
-  sleep 5
-done
-
-echo "\nReady"
+echo '----- Timeout waiting for cluster to be ready';
+exit 1;

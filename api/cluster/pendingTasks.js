260c7adfd84ef7fdfbb0ba5ca1978fe4d16b0648
nhtruong <644650+nhtruong@users.noreply.github.com>
2025-03-23 00:11:09 +0000

    Updated opensearch-js to reflect the latest OpenSearch API spec (2025-03-23)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/cluster/pendingTasks.js
+++ api/cluster/pendingTasks.js
@@ -26,7 +26,7 @@ or new allocations.
  * @memberOf API-Cluster
  *
  * @param {object} [params]
- * @param {string} [params.cluster_manager_timeout] - A duration. Units can be `nanos`, `micros`, `ms` (milliseconds), `s` (seconds), `m` (minutes), `h` (hours) and `d` (days). Also accepts "0" without a unit and "-1" to indicate an unspecified value.
+ * @param {string} [params.cluster_manager_timeout] - The amount of time to wait for a response from the cluster manager node. For more information about supported time units, see [Common parameters](https://opensearch.org/docs/latest/api-reference/common-parameters/#time-units).
  * @param {boolean} [params.local=false] - When `true`, the request retrieves information from the local node only. When `false`, information is retrieved from the cluster manager node.
  * @param {string} [params.master_timeout] DEPRECATED - A duration. Units can be `nanos`, `micros`, `ms` (milliseconds), `s` (seconds), `m` (minutes), `h` (hours) and `d` (days). Also accepts "0" without a unit and "-1" to indicate an unspecified value.
  *
--- api/cluster/postVotingConfigExclusions.js
+++ api/cluster/postVotingConfigExclusions.js
@@ -25,8 +25,8 @@ const { normalizeArguments } = require('../utils');
  * @memberOf API-Cluster
  *
  * @param {object} [params]
- * @param {string} [params.node_ids] - A comma-separated list of node IDs to exclude from the voting configuration. When using this setting, you cannot also specify `node_names`.
- * @param {string} [params.node_names] - A comma-separated list of node names to exclude from the voting configuration. When using this setting, you cannot also specify `node_ids`.
+ * @param {string} [params.node_ids] - A comma-separated list of node IDs to exclude from the voting configuration. When using this setting, you cannot also specify `node_names`. Either `node_ids` or `node_names` are required to receive a valid response.
+ * @param {string} [params.node_names] - A comma-separated list of node names to exclude from the voting configuration. When using this setting, you cannot also specify `node_ids`. Either `node_ids` or `node_names` are required to receive a valid response.
  * @param {string} [params.timeout] - When adding a voting configuration exclusion, the API waits for the specified nodes to be excluded from the voting configuration before returning a response. If the timeout expires before the appropriate condition is satisfied, the request fails and returns an error.
  *
  * @param {TransportRequestOptions} [options] - Options for {@link Transport#request}

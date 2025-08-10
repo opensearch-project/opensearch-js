55591b04ed0fffa7fb5ff2a929bc101e2814893d
nhtruong <644650+nhtruong@users.noreply.github.com>
2025-08-10 00:13:01 +0000

    Updated opensearch-js to reflect the latest OpenSearch API spec (2025-08-10)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ api/ltr/createModelFromSet.d.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
+
+/*
+ * This file was generated from the OpenSearch API Spec. Do NOT edit it
+ * manually. If you want to make changes, either update the spec or
+ * modify the API generator.
+ */
+
+import { ApiResponse } from '../../lib/Transport'
+import * as Common from '../_types/_common'
+import * as Global from '../_types/_global'
+
+export interface Ltr_CreateModelFromSet_Request extends Global.Params {
+  body: Ltr_CreateModelFromSet_RequestBody;
+  name: string;
+  routing?: string;
+  store?: string;
+}
+
+export type Ltr_CreateModelFromSet_RequestBody = Record<string, any>
+
+export interface Ltr_CreateModelFromSet_Response extends ApiResponse {
+  body: Ltr_CreateModelFromSet_ResponseBody;
+}
+
+export type Ltr_CreateModelFromSet_ResponseBody = Common.WriteResponseBase
+

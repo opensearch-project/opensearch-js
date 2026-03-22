502d9b1e37928379a7c1b992daff8df690ad152e
nhtruong <644650+nhtruong@users.noreply.github.com>
2026-03-22 00:16:46 +0000

    Updated opensearch-js to reflect the latest OpenSearch API spec (2026-03-22)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ api/searchRelevance/getStats.d.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
+
+/*
+ * This file was generated from the OpenSearch API Spec. Do NOT edit it
+ * manually. If you want to make changes, either update the spec or
+ * modify the API generator.
+ */
+
+import { ApiResponse } from '../../lib/Transport'
+import * as Common from '../_types/_common'
+import * as Global from '../_types/_global'
+import * as Nodes_Common from '../_types/nodes._common'
+
+export interface SearchRelevance_GetStats_Request extends Global.Params {
+  flat_stat_paths?: string;
+  include_all_nodes?: string;
+  include_individual_nodes?: string;
+  include_info?: string;
+  include_metadata?: string;
+  stat?: string;
+}
+
+export interface SearchRelevance_GetStats_Response extends ApiResponse {
+  body: SearchRelevance_GetStats_ResponseBody;
+}
+
+export interface SearchRelevance_GetStats_ResponseBody extends Nodes_Common.NodesResponseBase {
+  all_nodes?: Record<string, any>;
+  cluster_name?: Common.Name;
+  info?: Record<string, any>;
+  nodes: Record<string, any>;
+}
+

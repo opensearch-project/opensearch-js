a9197860554d57024b01f0845b32b6dcd48adf5e
Anqi Wang <anqiwa@amazon.com>
2026-04-23 14:22:37 -0700

    sign x-amz-content-sha256 header for aws sigV4 credential
    Signed-off-by: Anqi Wang <anqiwa@amazon.com>

--- test/unit/lib/aws/awssigv4signer.test.js
+++ test/unit/lib/aws/awssigv4signer.test.js
@@ -19,7 +19,7 @@ const { debug } = require('console');
 const { RequestAbortedError } = require('../../../../lib/errors');
 
 test('Sign with SigV4', (t) => {
-  t.plan(4);
+  t.plan(6);
 
   const mockCreds = {
     accessKeyId: uuidv4(),
@@ -58,6 +58,14 @@ test('Sign with SigV4', (t) => {
     'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   );
   t.same(signedRequest.service, 'es');
+
+  const authHeader = signedRequest.headers['Authorization'];
+  const signedHeadersMatch = authHeader.match(/SignedHeaders=([^,]+)/);
+  t.ok(signedHeadersMatch, 'Authorization header contains SignedHeaders');
+  t.ok(
+    signedHeadersMatch[1].includes('x-amz-content-sha256'),
+    'x-amz-content-sha256 is included in SignedHeaders'
+  );
 });
 
 test('Sign with SigV4 failure (with empty region)', (t) => {
